5154ce6519d93d7cadd599b6f74defda183718e2
Fabian Volkers <fabianvolkers@gmail.com>
2019-11-09 18:00:21 +0100

    added .eslintrc.js file

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports= {
+    "parser": "vue-eslint-parser",
+    "parserOptions": {
+        "ecmaVersion": 8,
+        "sourceType": "module",
+    },
+    rules: {
+        'no-console' : 'off',
+        'no-unused-vars' : 0
+    }
+}
